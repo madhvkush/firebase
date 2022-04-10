dba8afca6f4fd6d4aef7ad41a3eeadc2c3961649
Madhvendra Singh <madhvendra.singh@meridianlink.com>
2022-04-10 14:15:54 -0700

    Authentication using Firebase with React

---
+++ 1-react-firebase-auth/README.md
@@ -0,0 +1,14 @@
+# Getting Started Firebase
+
+Create your firebase Project
+https://console.firebase.google.com/
+
+Add you web App from here
+https://console.firebase.google.com/u/0/project/fir-react-6738c/overview
+https://console.firebase.google.com/u/0/project/fir-react-6738c/overview
+Add the firebase app script to your real App in firebaseConfig.js
+
+GoTo authentication
+https://console.firebase.google.com/u/0/project/fir-react-6738c/authentication/users
+Sign methods
+https://console.firebase.google.com/u/0/project/fir-react-6738c/authentication/providers
---
+++ 1-react-firebase-auth/src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// Please copy all the keys from the firebase-app
+const firebaseConfig = {
+  apiKey: "YourKey",
+  authDomain: "auth domain",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
